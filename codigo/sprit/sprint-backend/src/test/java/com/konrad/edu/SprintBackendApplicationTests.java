4244d6078d244864c5e23caaa7327d64fa49672f
DavidUK <36526045+c-esar@users.noreply.github.com>
2020-02-13 12:07:17 -0800

    Inicio de codigo

---
+++ codigo/sprit/sprint-backend/src/test/java/com/konrad/edu/SprintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konrad.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
