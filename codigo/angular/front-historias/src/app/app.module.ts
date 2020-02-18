0c304b8cb95a5d3b123f60670646ceebf996804d
DavidUK <36526045+c-esar@users.noreply.github.com>
2020-02-18 06:11:00 -0800

    Modulos generales app front

--- codigo/angular/front-historias/src/app/app.module.ts
+++ codigo/angular/front-historias/src/app/app.module.ts
@@ -8,17 +8,22 @@ import { MenuGeneralComponent } from './menu-general/menu-general.component';
 import { HeaderComponent } from './header/header.component';
 
 import {HttpClientModule} from '@angular/common/http';
-import {LoginService} from './login/login.service';
+import {LoginService} from './Servicios/login.service';
 import { RouterModule, Routes} from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AccordionModule} from 'primeng/accordion';
 import {MenuItem} from 'primeng/api';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {AuthGuard} from './guards/auth.guard';
+import { FormsModule } from '@angular/forms';
+import { ListaPersonasComponent } from './lista-personas/lista-personas.component';
+import { PersonaService } from './Servicios/persona.service';
 const routes: Routes = [
-  {path: '', redirectTo: '/menuPrincipal', pathMatch: 'full'}, //ruta principal de la aplicacion
+  {path: '', redirectTo: '/listPersonas', pathMatch: 'full'}, //ruta principal de la aplicacion
   // rutas secundarias se hace referencia con el component de cada uno
   {path:'login', component:LoginComponent},
-  {path:'menuPrincipal', component: MenuGeneralComponent}
+  {path:'menuPrincipal', component: MenuGeneralComponent, canActivate: [AuthGuard] },
+  {path:'listPersonas', component:ListaPersonasComponent}
 ];
 
 @NgModule({
@@ -27,17 +32,21 @@ const routes: Routes = [
     LoginComponent,
     FooterComponent,
     MenuGeneralComponent,
-    HeaderComponent
+    HeaderComponent,
+    ListaPersonasComponent
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     RouterModule.forRoot(routes),
     HttpClientModule,
     BrowserAnimationsModule,
-    AccordionModule
+    AccordionModule,
+    FormsModule
   ],
   providers: [
-    LoginService],
+    LoginService,
+    PersonaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
