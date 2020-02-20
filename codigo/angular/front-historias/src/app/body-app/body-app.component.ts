41c2473d8d40f19c66d0b532a363e48500c3cc7d
DavidUK <36526045+c-esar@users.noreply.github.com>
2020-02-20 06:04:09 -0800

    cuerpo principal de la aplicacion

---
+++ codigo/angular/front-historias/src/app/body-app/body-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-app',
+  templateUrl: './body-app.component.html',
+  styleUrls: ['./body-app.component.css']
+})
+export class BodyAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
