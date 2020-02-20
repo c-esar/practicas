f8531cf0805f642a47b864d92a6748ca4b4c2147
DavidUK <36526045+c-esar@users.noreply.github.com>
2020-02-20 05:55:51 -0800

    recursos del back-end

--- codigo/sprit/sprint-backend/src/main/resources/import.sql
+++ codigo/sprit/sprint-backend/src/main/resources/import.sql
@@ -1,2 +1,4 @@
 INSERT INTO hc_personas (nom_primer_nombre,nom_primer_apellido) values('Cesar','Diaz')
 INSERT INTO hc_personas (nom_primer_nombre,nom_primer_apellido) values('Carlos','Chacon')
+
+INSERT INTO hc_perfiles (nom_usuario,password,estado,nom_perfil) values('cesar','12345','activo','admin')
