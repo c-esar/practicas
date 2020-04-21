b1cca4a293c2c1934919aa9792671fab754ce9a6
DavidUK <36526045+c-esar@users.noreply.github.com>
2020-04-21 15:30:07 -0500

    Actualizacion documentos

---
+++ codigo/sprit/sprint-backend/src/main/java/com/konrad/edu/entity/Desencriptacion.java
@@ -0,0 +1,35 @@
+package com.konrad.edu.entity;
+
+import java.security.MessageDigest;
+import java.util.Arrays;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+public class Desencriptacion {
+
+	public static String Desencriptar(String textoEncriptado) throws Exception {
+
+		String secretKey = "qualityinfosolutions"; // llave para desenciptar datos
+		String base64EncryptedString = "";
+
+		try {
+			byte[] message = Base64.decodeBase64(textoEncriptado.getBytes("utf-8"));
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] digestOfPassword = md.digest(secretKey.getBytes("utf-8"));
+			byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+			SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+
+			Cipher decipher = Cipher.getInstance("DESede");
+			decipher.init(Cipher.DECRYPT_MODE, key);
+
+			byte[] plainText = decipher.doFinal(message);
+
+			base64EncryptedString = new String(plainText, "UTF-8");
+
+		} catch (Exception ex) {
+		}
+		return base64EncryptedString;
+	}
+}
